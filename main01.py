360248fb511e1535ab6bdfbb457819b1a2cde271
Lucas Silva Sarmento <141514378+Sarmentos@users.noreply.github.com>
2024-03-21 16:27:01 -0300

    Atividade-em-Python01

---
+++ main01.py
@@ -0,0 +1,5 @@
+primeiroNumero = int(input('Primeiro numero'))
+segundoNumero = int(input('Segundo numero'))
+soma = primeiroNumero + segundoNumero
+print(' A soma dos dois numeros são : {}',soma)
+
